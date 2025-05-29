a47360496562bf5d1eb1f7987a981c204a3c2d0c
PetroRosty <petrorosty@gmail.com>
2025-05-29 17:57:50 +0300

    feat: обновлен бот для Telegram авторизации на KalControlBot

--- src/components/TelegramLoginButton.tsx
+++ src/components/TelegramLoginButton.tsx
@@ -21,7 +21,7 @@ const TelegramLoginButton = () => {
     // Добавляем скрипт Telegram Login Widget
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
-    script.setAttribute('data-telegram-login', 'DietDiaryBot');
+    script.setAttribute('data-telegram-login', 'KalControlBot');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-radius', '8');
     script.setAttribute('data-request-access', 'write');
